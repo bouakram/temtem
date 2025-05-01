2e681076b75cfb2143e40b5e6bd4ff881b39d1c5
bouakram <kikoua203@gmail.com>
2025-05-01 13:24:20 +0100

    removing unssr animation import

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import 'react-native-reanimated';
+// import 'react-native-reanimated';
 // import 'react-native-gesture-handler';
 // import { Provider } from 'react-redux';
 // import { store } from './src/store/store';
